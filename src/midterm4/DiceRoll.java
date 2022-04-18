333bb6b3b2f79a67ed167589bede8bf62f4db31e
Tyler Conklin <tconklin@alumni.stanford.edu>
2022-04-18 13:22:11 -0700

    Add DiceRoll example.

---
+++ src/midterm4/DiceRoll.java
@@ -0,0 +1,33 @@
+package midterm4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiceRoll {
+
+    public static void main(String[] args) {
+        diceRoll(2);
+    }
+
+    private static void diceRoll(int numDice) {
+        diceRollHelper(numDice, new ArrayList<Integer>());
+    }
+
+    private static void diceRollHelper(int numDice, List<Integer> soFar) {
+        if (numDice == 0) {
+            System.out.println(soFar);
+        } else {
+            for (int i = 1; i <= 6; i++) {
+                // Choose
+                soFar.add(i);
+
+                // Explore
+                diceRollHelper(numDice - 1, soFar);
+
+                // Unchoose
+                soFar.remove(soFar.size() - 1);
+            }
+        }
+    }
+
+}
